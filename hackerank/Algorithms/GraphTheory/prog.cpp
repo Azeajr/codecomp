cca99e4d14edd0b6fd699505ff97aae3f53c61d5
Antonio Zea Jr <zeajantonio@gmail.com>
2022-05-14 14:41:10 -0400

    cleanup namespace

--- hackerank/Algorithms/GraphTheory/prog.cpp
+++ hackerank/Algorithms/GraphTheory/prog.cpp
@@ -1,54 +1,45 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 #include <unordered_map>
 #include <utility>
+#include <vector>
 
-using namespace std;
- 
- 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
-    int n, m, k;
-    std::cin >> n >> m >> k;
-    
-    std::unordered_map<string, std::pair<int, int>> destinations;
-    
-    string destination;
-    int destinationCost;
-    for(int i = 0; i < k; i++){
-        std:cin >> destination >> destinationCost;
-        destinations[destination]=std::pair<int, int>(destinationCost,0);
-    }
-    
-    string customers[n];
-    
-    for(int i = 0; i < n; i++){
-        // std::getline(std::cin, customers[i]);
-        cin >> customers[i];
-        destinations[customers[i]].second++;
-    }
-    
-    cout << n << m << k << endl;
-    
-    for(const auto& d : destinations){
-        cout << "Destination:["<< d.first << "] Cost:["<<d.second.first<<"] Count:["<<d.second.second<< "]"<<endl;
-    }
-    
-    for(string x: customers){
-        cout << x << endl;
-    }
-    
-    int customersWindows[n];
-    
-    
-    
-    return 0;
-}
- 
- 
- 
- 
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  int n, m, k;
+  std::cin >> n >> m >> k;
 
+  std::unordered_map<std::string, std::pair<int, int>> destinations;
+
+  std::string destination;
+  int destinationCost;
+  for (int i = 0; i < k; i++) {
+    std::cin >> destination >> destinationCost;
+    destinations[destination] = std::pair<int, int>(destinationCost, 0);
+  }
+
+  std::string customers[n];
+
+  for (int i = 0; i < n; i++) {
+    // std::getline(std::cin, customers[i]);
+    std::cin >> customers[i];
+    destinations[customers[i]].second++;
+  }
+
+  std::cout << n << m << k << std::endl;
+
+  for (const auto &d : destinations) {
+    std::cout << "Destination:[" << d.first << "] Cost:[" << d.second.first
+              << "] Count:[" << d.second.second << "]" << std::endl;
+  }
+
+  for (std::string x : customers) {
+    std::cout << x << std::endl;
+  }
+
+  int customersWindows[n];
+
+  return 0;
+}
