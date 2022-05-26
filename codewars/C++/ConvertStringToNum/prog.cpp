53a9cbee2d8e1dd782068d9b712c5c307c25faa4
Antonio Zea Jr <zeajantonio@gmail.com>
2022-05-26 10:20:40 -0400

    improvment by sticking to the STD

--- codewars/C++/ConvertStringToNum/prog.cpp
+++ codewars/C++/ConvertStringToNum/prog.cpp
@@ -2,21 +2,7 @@
 #include <string>
 
 int string_to_number(const std::string &s) {
-  double number = 0.0;
-  bool neg = false;
-
-  for (auto var : s) {
-    if (var == '-') {
-      neg = true;
-    } else {
-      number = (number * 10) + (var - 48);
-    }
-  }
-  if (neg) {
-    number *= -1;
-  }
-
-  return number;
+  return std::stoi(s);
 }
 
 int main(int argc, char const *argv[]) {
