182c8b59d2f1da4d7c5ca213ac4539dca9ac5e42
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-10 23:11:08 -0400

    add multiply_strings function
    Much simpler to implement long division method vs faster large integer
    methods

--- codewars/C++/DivideIntAsStr/prog.cpp
+++ codewars/C++/DivideIntAsStr/prog.cpp
@@ -1,10 +1,38 @@
-#include <algorith>
+// #include <algorith>
 #include <iostream>
 #include <string>
 #include <vector>
 
-std::vector<std::string> multiply_strings(std::string a, std::string b) {
-  int m = std::min(a.size(), multiplier.size());
+std::string multiply_strings(std::string a, std::string b) {
+  std::string product = "";
+  int interimProduct;
+  int carry = 0;
+
+  for (int digitB = b.length() - 1; digitB >= 0; digitB--) {
+    for (int digitA = a.length() - 1; digitA >= 0; digitA--) {
+      interimProduct = (b[digitB] - '0') * (a[digitA] - '0') + carry;
+      carry = interimProduct / 10;
+      interimProduct %= 10;
+
+      product.insert(0, std::to_string(interimProduct));
+    }
+  }
+
+  if (carry > 0) {
+    product.insert(0, std::to_string(carry));
+  }
+  
+  return product;
+
+  // for (int &&digitB : b)
+  // {
+  //   for (auto &&digitA : a)
+  //   {
+  //     interimProduct = (digitB - '0') * (digitA - '0');
+  //     carry = interimProduct / 10;
+  //     interimProduct %= 10;
+  //   }
+  // }
 
   // return std::vector<std::string>{quotient, a};
 }
@@ -13,11 +41,11 @@ int main(int argc, char const *argv[]) {
   std::string multiplicand = argv[1];
   std::string multiplier = argv[2];
 
-  int std::min(multiplicand.size(), multiplier.size());
+  std::cout << multiply_strings(argv[1], argv[2]) << std::endl;
 
-  for (auto x : multiply_strings(argv[1], argv[2])) {
-    std::cout << x << std::endl;
-  }
+  // for (auto x : multiply_strings(argv[1], argv[2])) {
+  //   std::cout << x << std::endl;
+  // }
 
   // std::string temp1 = argv[1];
   // std::string temp2 = argv[2];
