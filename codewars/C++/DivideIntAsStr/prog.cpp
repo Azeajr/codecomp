6852e4d2cd7691379ffcb198832f6c610cb51aa4
Antonio Zea Jr <zeajantonio@gmail.com>
2022-07-25 21:07:28 -0400

    add multiplyStringsByDigit, integerStringsCompare

--- codewars/C++/DivideIntAsStr/prog.cpp
+++ codewars/C++/DivideIntAsStr/prog.cpp
@@ -2,64 +2,83 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <string_view>
 
-std::string multiply_strings(std::string a, std::string b) {
+
+std::string multiplyStringsByDigit(std::string a, int b) {
+  if (b < 0 || b > 9)
+    return nullptr;
   std::string product = "";
   int interimProduct;
   int carry = 0;
 
-  for (int digitB = b.length() - 1; digitB >= 0; digitB--) {
-    for (int digitA = a.length() - 1; digitA >= 0; digitA--) {
-      interimProduct = (b[digitB] - '0') * (a[digitA] - '0') + carry;
-      carry = interimProduct / 10;
-      interimProduct %= 10;
+  for (int digitA = a.length() - 1; digitA >= 0; digitA--) {
+    interimProduct = b * (a[digitA] - '0') + carry;
+    carry = interimProduct / 10;
+    interimProduct %= 10;
 
-      product.insert(0, std::to_string(interimProduct));
-    }
+    product.insert(0, std::to_string(interimProduct));
   }
 
   if (carry > 0) {
     product.insert(0, std::to_string(carry));
   }
-  
+
   return product;
+}
 
-  // for (int &&digitB : b)
-  // {
-  //   for (auto &&digitA : a)
-  //   {
-  //     interimProduct = (digitB - '0') * (digitA - '0');
-  //     carry = interimProduct / 10;
-  //     interimProduct %= 10;
-  //   }
-  // }
+int integerStringsCompare(std::string *a, std::string *b) {
+  if ((*a).length() > (*b).length()) {
+    return 1;
+  } else if ((*a).length() < (*b).length()) {
+    return -1;
+  } else {
+    for (int i = 0; i < (*a).length(); i++) {
+      if ((*a)[i] > (*b)[i]) {
+        return 1;
+      } else if ((*a)[i] < (*b)[i]) {
+        return -1;
+      }
+    }
+    return 0;
+  }
+}
 
-  // return std::vector<std::string>{quotient, a};
+int largerIntegerFinder(std::string smaller, std::string larger, int index) {
+
+  return 0;
+}
+
+std::vector<std::string> divide_strings(std::string *a, std::string *b) {
+  std::string quotient = "0";
+  std::string remainder = "0";
+
+  int comp = integerStringsCompare(a, b);
+  if (comp == -1) {
+    return std::vector<std::string>{quotient, *a};
+  } else if (comp == 0) {
+    return std::vector<std::string>{"1", remainder};
+  }
+
+  int start = 0;
+  for (int i = start; i < start + (*b).length() - 1; i++) {
+  }
+
+  return std::vector<std::string>{quotient, remainder};
 }
 
 int main(int argc, char const *argv[]) {
-  std::string multiplicand = argv[1];
-  std::string multiplier = argv[2];
+  std::string first = "1234";
+  std::string second = "12";
 
-  std::cout << multiply_strings(argv[1], argv[2]) << std::endl;
+  for (auto x : divide_strings(&first, &second)) {
+    std::cout << x << std::endl;
+  }
 
-  // for (auto x : multiply_strings(argv[1], argv[2])) {
-  //   std::cout << x << std::endl;
-  // }
+  std::string str = "TheBigStr";
+  std::string_view p1 = std::string_view(str.data() + 3, 3);
 
-  // std::string temp1 = argv[1];
-  // std::string temp2 = argv[2];
-
-  // std::cout << temp1 << "\t" << temp2 << std::endl;
-  // subtract_strings(&temp1, &temp2);
-
-  // std::cout << temp1 << "\t" << temp2 << std::endl;
-
-  // std::cout << temp1 << std::endl;std::cout << temp1 << std::endl;
-  // increment_string(&temp1);
-  // s
-  // increment_string(&temp1);
-  // std::cout << temp1 << std::endl;
+  std::cout << p1;
 
   return 0;
 }
